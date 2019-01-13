8d668fafe5ecb4c390229d81e1d387c76ef3f9c6
Rabeh <rabih23r@gmail.com>
2019-01-13 17:42:09 +0300

    flask-hello-world

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 # --- Flask Hello World --- #
 
 # import the flask class from the falsk package 
+
 from flask import Flask 
 
 # create the application object 
 app = Flask(__name__)
+
+#error handling 
+app.config["DEBUG"] = True 
 
 # use the decorator pattern to 
 # link the view function to a url 
@@ -13,7 +17,39 @@ app = Flask(__name__)
 
 # define the view usign a function, which returns a string 
 def hello_world():
-	return "Hello, World!"
+	return "Hello, World!?!?!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+# a dynamic route that only accepts integer value
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print(value + 1)
+	return "correct"
+
+# a dynamic route that only accepts float value 
+@app.route("/float/<float:value>")
+def float_type(value):
+	print(value + 1)
+	return "correct"
+
+# a dynamic route that accepts slashes 
+# You can use both integers and floats as well, but they will be converted to unicode
+@app.route("/path/<path:value>")
+def path_type(value):
+	print(value)
+	return "correct"
+
+# Here, the route can take an optional parameter of name, and the response object is
+#dependent on the assigned value
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael" :
+		return "Hello, {}".format(name)
+	else :
+		return "Not Found", 404
 
 # start the development server using the run() method 
 if __name__ == "__main__":
